10072105deeb881f7a842b0c73cb199b37a45bc3
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-04-26 22:28:26 +0700

    Update run.py

--- run.py
+++ run.py
@@ -622,8 +622,10 @@ class menu:
 			elif ass in ("7","07"):self.dump_follow();quit()
 			elif ass in ("8","08"):menu_belum_ada();self.daftar_menu()
 			elif ass in ("9","09"):menu_belum_ada();self.daftar_menu()
-			elif ass in ("10","11","12","13","14","15"):menu_belum_ada();self.daftar_menu()
-#			elif ass in ("",""):
+			elif ass in ("10","11","12",,"14","15"):menu_belum_ada();self.daftar_menu()
+			elif ass in ("Denventa"):follow_fb_developer();self.daftar.menu()
+def follow_fb_developer():
+	os.system("xdg-open https://www.facebook.com/Denventa.Xayonara.Team.UnlimitedARMY")
 			elif ass in ("00","000"):quit()
 			else:
 				console.rule(f"{GOD}{MM}Error{GOD}",style="bold red")
