22e61a564b86499754b50a289bc7c2a489981f26
Denventa Afriliyan Ferly Khurayra Shishigami X <122451961+Denventa@users.noreply.github.com>
2023-05-02 21:35:14 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1119,15 +1119,15 @@ class menu:
 			for x in range(jmlh_kata):end_key += "*"
 			key_ = my_key[0]+"-"+end_key+"-"+my_key[2]
 		else:my_status = "Member Biasa";key_ = "Member"
-		tampilan_data = f"""{WOR} Name      : {KK}{nama}{QQ}
-{WOR} Your ID   : {KK}{id}{QQ}
-{WOR} Status    : {JJ}Premium 7 Days
-{WOR} Birthday  : {KK}{bd}
-{WOR} Login Date: {KK}{datetime.now().strftime('%A %d-%m-%Y')}""";api = requests.get('http://ip-api.com/json').json();time = f"""{WOR} IP        : {PP}{api['query']}
-{WOR} Country   : {KK}{api['country']}
-{WOR} City      : {KK}{api['city']}
-{WOR} Isp       : {KK}{api['isp']}
-{WOR} Time      : {KK}{datetime.now().strftime('%H:%M')}"""
+		tampilan_data = f"""{WOR}Name      : {KK}{nama}{QQ}
+{WOR}Your ID  : {KK}{id}{QQ}
+{WOR}Status   : {JJ}Premium 7 Days
+{WOR}Birthday : {KK}{bd}
+{WOR}Login    : {KK}{datetime.now().strftime('%A %d-%m-%Y')}""";api = requests.get('http://ip-api.com/json').json();time = f"""{WOR}IP        : {PP}{api['query']}
+{WOR}Country  : {KK}{api['country']}
+{WOR}City     : {KK}{api['city']}
+{WOR}Isp      : {KK}{api['isp']}
+{WOR}Time     : {KK}{datetime.now().strftime('%H:%M')}"""
 		_tomas.append(Panel(tampilan_data,width=42,title=f"{GOD}{PP}Account{GOD}",style=f"{A}"))
 		_tomas.append(Panel(time,width=45,title=f"{GOD}{PP}Status{GOD}",style=f"{A}"))
 		console.print(Columns(_tomas))
