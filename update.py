accfd4129f20a012c56f134c599717ae3b7ff184
[ Dvanmeploph - ( Ferly Afriliyan - Itsmeafriliyan ) ] - <122451961+Livian-xyz@users.noreply.github.com>
2023-06-11 19:23:05 +0700

    Update update.py

--- update.py
+++ update.py
@@ -9,7 +9,7 @@ mv data $HOME
 mv results $HOME
 cd
 rm -rf DARK-FB
-git clone https://github.com/Itsmeafriliyan/DARK-FB
+git clone https://github.com/Livian-xyz/DARK-FB
 cd $HOME
 mv data DARK-FB
 mv results DARK-FB
@@ -21,5 +21,5 @@ os.system(Dvanmeploph)
 print(' |')
 print(' |')
 print(' |-->Update Script Sudah Selesai...')
-print(' |-->Silahkan Jalankan Perintah Ini : python main.cpp')
+print(' |-->Silahkan Jalankan Perintah Ini : python main.py')
 os.sys.exit()
